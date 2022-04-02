349eb21779d8f605fe25e05c94175bbb67b4d78e
sercanbayrambey <sercanbayrambeyy@gmail.com>
2022-04-02 13:23:22 +0300

    init

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp'); 
+const imagemin = require('gulp-imagemin');
+
+function defaultTask(cb) {
+    // place code for your default task here
+    cb();
+}
+
+exports.default = defaultTask
+
+gulp.task('imagemin', function () {
+    return gulp.src('images/*').pipe(imagemin()).pipe(gulp.dest('result/'));
+});
