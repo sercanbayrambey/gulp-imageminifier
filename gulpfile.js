0d615a43d310a6d7ad3e8f8cbbef7df3be30c294
Sercan Bayrambey <sercanbayrambeyy@gmail.com>
2023-10-02 10:16:34 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
-var gulp = require('gulp'); 
+var gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const debug = require('gulp-debug');
+var imageResize = require('gulp-image-resize');
 
 function defaultTask(cb) {
     // place code for your default task here
@@ -9,5 +12,22 @@ function defaultTask(cb) {
 exports.default = defaultTask
 
 gulp.task('imagemin', function () {
-    return gulp.src('images/**/*').pipe(imagemin()).pipe(gulp.dest('result/'));
+    return gulp.src('images/**/*')
+
+        .pipe(debug({ title: 'minify start for:' }),)
+       /*  .pipe(imageResize({
+            width: 1440,
+            crop: false,
+            upscale: true,
+            imageMagick: true
+        })) */
+        .pipe(debug({ title: 'resized image:' }),)
+        .pipe(
+            imagemin([
+                imagemin.optipng({ optimizationLevel: 5 }),
+                imagemin.mozjpeg({ quality: 85, progressive: true }),
+            ]))
+        //.pipe(webp())
+        .pipe(gulp.dest('result/'))
+        .pipe(debug({ title: 'minified::' }),)
 });
